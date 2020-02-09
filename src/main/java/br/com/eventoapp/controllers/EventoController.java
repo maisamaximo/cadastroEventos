59572e494de142f1950767a504ae24e3a3ead4ec
Maisa Maximo <maisa_maximo@outlook.com>
2020-02-09 15:15:14 -0300

    add event list

--- src/main/java/br/com/eventoapp/controllers/EventoController.java
+++ src/main/java/br/com/eventoapp/controllers/EventoController.java
@@ -1,11 +1,12 @@
 package br.com.eventoapp.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.eventoapp.models.Evento;
 import br.com.eventoapp.repository.EventoRepository;
@@ -26,6 +27,15 @@ public class EventoController {
 		
 		eventoRepository.save(evento);
 		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping(value = "/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		mv.addObject("eventos", eventos);
+		
+		return mv;
 	}
 	
 }
