29dd6f2aa41102bd8aff7176908dac02b1d04954
Maisa Maximo <maisa_maximo@outlook.com>
2020-02-09 01:02:28 -0300

    create repository

---
+++ src/main/java/br/com/eventoapp/repository/EventoRepository.java
@@ -0,0 +1,8 @@
+package br.com.eventoapp.repository;
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.eventoapp.models.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, String>{
+
+}
